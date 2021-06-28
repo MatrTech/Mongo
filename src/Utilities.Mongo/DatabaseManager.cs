571f22f0f9f7f301450741f11c6be3edc5a0b455
Mark Visschers <mark.visschers@connecthing.tech>
2021-06-28 13:08:05 +0200

    adding create method

--- src/Utilities.Mongo/DatabaseManager.cs
+++ src/Utilities.Mongo/DatabaseManager.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using MatrTech.Utilities.Mongo.Models;
+using MongoDB.Driver;
 
 namespace MatrTech.Utilities.Mongo
 {
     public class DatabaseManager
     {
+        public static IMongoDatabase Create<TContext>(string databaseConfigName)
+            where TContext : MongoContext
+        {
+            var client = new MongoClient("");
+            return client.GetDatabase(databaseConfigName);
+        }
     }
 }
